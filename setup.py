d81f73e1fa36d0d5449598a5fa0d7e0be7110b9d
dtsukiyama <davidtsukiyama1@gmail.com>
2019-06-28 07:25:22 -0400

    update license

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name='muri',
     version='0.1',
-    description='Scale Images',
+    description='Scale and Denoise Images Easily',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='David Tsukiyama',
+    author_email="davidtsukiyama1@gmail.com",
     modules=['muda','yare','kantan','maji'],
     packages=find_packages(),
     include_package_data=True,
